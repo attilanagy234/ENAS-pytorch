e99261af24f2851f45910342b3305a6f2b235b8b
Attila Nagy <attila.nagy234@gmail.com>
2019-10-19 14:23:07 +0200

    fixed .sh script

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+set -e
+
+TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+
+echo "######################################"
+echo "BUILDING IMAGE WITH TAG $TIMESTAMP"
+echo "######################################"
+
+docker build -t enas-tdk .
+
+docker tag enas-tdk:latest 855453161598.dkr.ecr.eu-west-1.amazonaws.com/enas-tdk:latest
+docker tag enas-tdk:latest 855453161598.dkr.ecr.eu-west-1.amazonaws.com/enas-tdk:$TIMESTAMP
+
+docker push 855453161598.dkr.ecr.eu-west-1.amazonaws.com/enas-tdk:latest
+docker push 855453161598.dkr.ecr.eu-west-1.amazonaws.com/enas-tdk:$TIMESTAMP
